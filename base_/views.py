dd7a3c859d40da1df27765e9a5ea3283f5cb3db6
Muhammed Answar C K <answarc764@gmail.com>
2025-02-15 13:20:14 +0530

    minor change

--- base_/views.py
+++ base_/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from utils.utils import get_pickup_line
-# import time
+
 def home_view(request):
     context = {}
     if request.method == 'POST':
-        # context = {"pickupline" : get_pickup_line()}
-        # time.sleep(5)
-        context = {"pickupline": "HEY"}
+        context = {"pickupline" : get_pickup_line()}
         
     return render(request, 'index.html', context)
